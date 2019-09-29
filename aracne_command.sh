2120e9bc116b2f9092a0614bf47b30922daedbf2
paul-michaelis <55940514+paul-michaelis@users.noreply.github.com>
2019-09-29 09:01:21 +0200

    Delete aracne_command.sh

--- aracne_command.sh
+++
@@ -1,3 +0,0 @@
-
-java -jar aracne2.jar -i /home/michaelp/Downloads/aracne/aracne_in/124_aracne_all.txt -o /home/michaelp/Downloads/aracne/124_aracne_all_out_2.txt -a accurate -m ct -t 0.1 -e 0.2 -l TFS.Danio_rerio_ARACNE_embryo2016_12_09_16_43.txt
-
